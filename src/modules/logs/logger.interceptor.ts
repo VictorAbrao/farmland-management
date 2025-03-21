7fd0bf059f95a7c519c537c5401a809062356bab
victor abrao <victor.abrao@globmail.com.br>
2025-03-21 18:53:19 -0300

    update log

---
+++ src/modules/logs/logger.interceptor.ts
@@ -0,0 +1,24 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, tap } from 'rxjs';
+import { LoggerService } from './logger.service';
+
+@Injectable()
+export class LoggerInterceptor implements NestInterceptor {
+  constructor(private logger: LoggerService) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest();
+    const method = request.method;
+    const url = request.url;
+    return next.handle().pipe(
+      tap(() => {
+        this.logger.log(method + ' ' + url);
+      }),
+    );
+  }
+}
---
+++ src/modules/logs/logger.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import * as winston from 'winston';
+import 'winston-daily-rotate-file';
+
+@Injectable()
+export class LoggerService {
+  private logger: winston.Logger;
+
+  constructor() {
+    this.logger = winston.createLogger({
+      level: 'info',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+      ),
+      transports: [
+        new winston.transports.DailyRotateFile({
+          dirname: 'logs',
+          filename: 'app-%DATE%.log',
+          datePattern: 'YYYY-MM-DD',
+          zippedArchive: true,
+        }),
+        new winston.transports.Console(),
+      ],
+    });
+  }
+
+  log(message: string) {
+    this.logger.info(message);
+  }
+
+  error(message: string) {
+    this.logger.error(message);
+  }
+}
